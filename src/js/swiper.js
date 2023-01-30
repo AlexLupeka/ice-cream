76faaa1fa6c4f30b1ba52abfcfa2711e09b69093
AlexLupeka <lupeka.alex@gmail.com>
2023-01-30 15:05:30 +0200

    add project files

---
+++ src/js/swiper.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+
+    clickable: true,
+
+    dynamicBullets: true,
+  },
+
+  simulateTouch: true,
+  grabCursor: true,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  mousewheel: {
+    sensitivity: 1,
+  },
+
+  initialSlide: 2,
+
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false,
+  // },
+  speed: 800,
+});
